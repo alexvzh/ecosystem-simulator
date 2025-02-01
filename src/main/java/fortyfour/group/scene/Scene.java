d69e85bf16eb2aaf9cf84b33e4b3c2b013b35c5a
alexvhz <alexvictor1305@gmail.com>
2025-02-02 00:49:27 +0100

    Add abstract update() and draw() methods to Scene class

--- src/main/java/fortyfour/group/scene/Scene.java
+++ src/main/java/fortyfour/group/scene/Scene.java
@@ -1,4 +1,10 @@
 package fortyfour.group.scene;
 
-public class Scene {
+import java.awt.*;
+
+public abstract class Scene {
+
+    public abstract void update();
+    public abstract void draw(Graphics2D g2d);
+
 }
