54cd0459343e556676a3400b4017fcfd97a36001
alexvhz <alexvictor1305@gmail.com>
2025-02-02 20:47:07 +0100

    Create MenuScene class

---
+++ src/main/java/fortyfour/group/scene/scenes/MenuScene.java
@@ -0,0 +1,19 @@
+package fortyfour.group.scene.scenes;
+
+import fortyfour.group.scene.Scene;
+
+import java.awt.*;
+
+public class MenuScene extends Scene {
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(Graphics2D g2d) {
+
+    }
+
+}
