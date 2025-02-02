fa811fa05d0f39996a331caa6cb1dd2967248163
alexvhz <alexvictor1305@gmail.com>
2025-02-02 21:07:08 +0100

    Add basic functionality to Entity class

--- src/main/java/fortyfour/group/entity/Entity.java
+++ src/main/java/fortyfour/group/entity/Entity.java
@@ -2,4 +2,26 @@ package fortyfour.group.entity;
 
 public abstract class Entity {
 
+    private int x, y;
+
+    public Entity(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
