5db89af79a7bed87a5017f2d49bec99868c71d3e
alexvhz <alexvictor1305@gmail.com>
2025-02-02 01:12:33 +0100

    Make Scene extend JPanel and set up initial properties

--- src/main/java/fortyfour/group/scene/Scene.java
+++ src/main/java/fortyfour/group/scene/Scene.java
@@ -1,8 +1,19 @@
 package fortyfour.group.scene;
 
+import javax.swing.*;
 import java.awt.*;
 
-public abstract class Scene {
+public abstract class Scene extends JPanel {
+
+    public Scene() {
+
+        this.setPreferredSize(new Dimension(640, 640));
+        this.setBackground(Color.GRAY);
+        this.setDoubleBuffered(true);
+        this.setFocusable(true);
+        this.setLayout(null);
+
+    }
 
     public abstract void update();
     public abstract void draw(Graphics2D g2d);
