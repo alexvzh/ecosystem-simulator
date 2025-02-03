afc6b84fd958d603282c26e6f022c6330ae8ce1b
alexvhz <alexvictor1305@gmail.com>
2025-02-03 16:51:32 +0100

    Create and init window

--- src/main/java/fortyfour/group/main/Main.java
+++ src/main/java/fortyfour/group/main/Main.java
@@ -1,3 +1,24 @@
 package fortyfour.group.main;
 
-public class Main { public static void main(String[] args) {} }
+import fortyfour.group.scene.scenes.MenuScene;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        JFrame window = new JFrame();
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("ecosystem-simulator");
+
+        window.getContentPane().add(new MenuScene());
+        window.pack();
+        window.setLocationRelativeTo(null);
+
+        window.setVisible(true);
+
+    }
+}
