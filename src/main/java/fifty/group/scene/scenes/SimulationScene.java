31dce81577bb481c03594e5ad1305e32849d5498
alexvhz <alexvictor1305@gmail.com>
2025-02-10 18:34:51 +0100

    Create SimulationScene class

---
+++ src/main/java/fifty/group/scene/scenes/SimulationScene.java
@@ -0,0 +1,22 @@
+package fifty.group.scene.scenes;
+
+import fifty.group.scene.Scene;
+
+import java.awt.*;
+
+public class SimulationScene extends Scene {
+
+    public SimulationScene() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(Graphics2D g2d) {
+
+    }
+}
